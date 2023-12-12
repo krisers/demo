518565255aadcf49611b900b81f6fd841c90c1ee
krisers <kriserstream@gmail.com>
2023-12-12 20:15:57 +0300

    sample2

--- utils.py
+++ utils.py
@@ -5,6 +5,7 @@ import numpy as np
 import time
 import scipy
 import copy
+import librosa
 import matplotlib.pyplot as plt
 
 
@@ -188,4 +189,26 @@ class Image_Caption():
         print(f'Time passed kp: {time.time()-t0}')
 
         return len(good)
+    
+
+    
+def get_key(audio_file_path):
+
+    y, sr = librosa.load(audio_file_path)
+
+    # Compute the Chroma Short-Time Fourier Transform (chroma_stft)
+    chromagram = librosa.feature.chroma_stft(y=y, sr=sr)
+
+    # Calculate the mean chroma feature across time
+    mean_chroma = np.mean(chromagram, axis=1)
+
+    # Define the mapping of chroma features to keys
+    chroma_to_key = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
+
+    # Find the key by selecting the maximum chroma feature
+    estimated_key_index = np.argmax(mean_chroma)
+    estimated_key = chroma_to_key[estimated_key_index]
+
+    # Print the detected key
+    print("Detected Key:", estimated_key)
 
